6b71d486b00d5243280a738d39b69ab5a5b4a497
vkoschman <207955@niuitmo.ru>
2019-11-17 02:13:38 +0300

    lab 3 completed

--- Scripts/task1.py
+++ Scripts/task1.py
@@ -2,87 +2,162 @@ import numpy as np
 import scipy.optimize as sp
 import functools as fun
 from mpmath import *
+from numpy import linalg
+from scipy.optimize import least_squares
 
-from Scripts.utility import golden_section, epsilon, dataset, alpha, betta
+from Scripts.utility import golden_section, epsilon, dataset, alpha, betta, gd_epsilon, plot_dataset, \
+    plot_functional_history
 
-linear_reg_f = lambda x, a, b: a * x + b
-rational_reg_f = lambda x, a, b: a / (1 + b * x)
+linear_reg_f = lambda a, b: np.array([a * dataset[0][i] + b for i in range(len(dataset[0]))])
+rational_reg_f = lambda a, b: np.array([a / (1 + b * dataset[0][i]) for i in range(len(dataset[0]))])
 stop_condition = lambda previous, current: np.linalg.norm(previous - current) < epsilon
-functional_linear = lambda a, b: np.sum(
-    np.array([np.square(a * dataset[0][i] + b - dataset[1][i][0]) for i in range(len(dataset[0]))]))
-functional_rational = lambda a, b: np.sum(
-    np.array([np.square(a / (1 + b * dataset[0][i]) - dataset[1][i][0]) for i in range(len(dataset[0]))]))
+functional_linear = lambda point: np.sum(
+    np.array([np.square(point[0] * dataset[0][i] + point[1] - dataset[1][i][0]) for i in range(len(dataset[0]))]))
+functional_rational = lambda point: np.sum(
+    np.array([np.square(point[0] / (1 + point[1] * dataset[0][i]) - dataset[1][i][0]) for i in range(len(dataset[0]))]))
 update_function = lambda param, dfdparam, lr: param - lr * dfdparam
+
+# with 2 parameters for taking partial derivatives
+functional_linear2 = lambda a, b: np.sum(
+    np.array([np.square(a * dataset[0][i] + b - dataset[1][i][0]) for i in range(len(dataset[0]))]))
+functional_rational2 = lambda a, b: np.sum(
+    np.array([np.square(a / (1 + b * dataset[0][i]) - dataset[1][i][0]) for i in range(len(dataset[0]))]))
 
 
 def steepest_gradient_descent(functional):
-    previous = np.array([np.random.rand(1)[0], np.random.rand(1)[0]])  # starting point
+    prev = np.array([np.random.rand(1)[0], np.random.rand(1)[0]])  # starting point
     iterations = 0
+    functional_history = []
     while True:
-        df_da = diff(functional, (previous[0], previous[1]), (1, 0))
-        df_db = diff(functional, (previous[0], previous[1]), (0, 1))
+        df_da = diff(functional, (prev[0], prev[1]), (1, 0))
+        df_db = diff(functional, (prev[0], prev[1]), (0, 1))
         gradient = np.array([df_da, df_db])
-        left, right = golden_section(fun.partial(update_function, param=previous[0], dfdparam=df_da), (0.0001, 0.1))
-        optimal_step_a = 0.5 * (left + right)
-        left, right = golden_section(fun.partial(update_function, param=previous[1], dfdparam=df_db), (0.0001, 0.1))
-        optimal_step_b = 0.5 * (left + right)
+
+        left1, right1 = golden_section(fun.partial(update_function, param=prev[0], dfdparam=df_da), (0.00001, 0.001))
+        optimal_step_a = 0.5 * (left1 + right1)
+        left2, right2 = golden_section(fun.partial(update_function, param=prev[1], dfdparam=df_db), (0.00001, 0.001))
+        optimal_step_b = 0.5 * (left2 + right2)
         optimal_steps = np.array([optimal_step_a, optimal_step_b])
-        current = previous - optimal_steps * gradient
+
+        curr = prev - optimal_steps * gradient
         iterations += 1
-        if stop_condition(previous, current):
+        functional_history.append(functional(curr[0], curr[1]))
+        if stop_condition(prev, curr):
             break
         else:
-            previous = current
-    return current, iterations
+            prev = curr
+    # plot_functional_history(iterations, functional_history)
+    return curr, iterations
 
 
 def gradient_descent(functional):
-    previous = np.array([np.random.rand(1)[0], np.random.rand(1)[0]])  # starting point
+    prev = np.array([np.random.rand(1)[0], np.random.rand(1)[0]])  # starting point
     iterations = 0
-    lr = 0.005
+    functional_history = []
+    lr = 0.0001
     while True:
-        df_da = diff(functional, (previous[0], previous[1]), (1, 0))
-        df_db = diff(functional, (previous[0], previous[1]), (0, 1))
+        df_da = float(diff(functional, (prev[0], prev[1]), (1, 0)))
+        df_db = float(diff(functional, (prev[0], prev[1]), (0, 1)))
         gradient = np.array([df_da, df_db])
-        current = previous - lr * gradient
+        curr = prev - lr * gradient
         iterations += 1
-        if stop_condition(previous, current):
+        functional_history.append(functional(curr[0], curr[1]))
+        if stop_condition(prev, curr):
             break
         else:
-            previous = current
-    return current, iterations
+            prev = curr
+    # plot_functional_history(iterations, functional_history)
+    return curr, iterations
 
 
 def newton_method(functional):
-    return None, None
+    prev = np.array([np.random.rand(1)[0], np.random.rand(1)[0]])  # starting point
+    iterations = 0
+    while True:
+        df_da = diff(functional, (prev[0], prev[1]), (1, 0))
+        df_db = diff(functional, (prev[0], prev[1]), (0, 1))
+        gradient_f = np.array([df_da, df_db])
+        hessian_f = None  # haven't found any normal way to compute it (maybe go and use pytorch (haha classic))
+        curr = prev - hessian_f ** (-1) * gradient_f
+        iterations += 1
+        if stop_condition(prev, curr):
+            break
+        else:
+            prev = curr
+    return curr, iterations
 
 
-def levenberg_marquardt_algorithm(functional):
-    return None, None
+def levenberg_marquardt_algorithm(functional, regression):
+    prev = np.array([np.random.rand(1)[0], np.random.rand(1)[0]])  # starting point
+    iterations = 0
+    lr = np.random.rand(1)[0]
+    factor = 2  # damping factor should be > 1
+    k = 1  # "some k" according to wiki
+    while True:
+        while True:
+            df_da = diff(regression, (prev[0], prev[1]), (1, 0))
+            df_db = diff(regression, (prev[0], prev[1]), (0, 1))
+            gradient_f = np.array(
+                [[float(df_da[i]) for i in range(len(df_da))], [float(df_db[i]) for i in range(len(df_da))]])
+            j_jT = gradient_f.dot(gradient_f.transpose())
+            H_f = j_jT + lr * np.ones((len(j_jT), len(j_jT)))
+            f_s = regression(np.array(prev[0]), np.array(prev[1]))
+            y = np.concatenate(dataset[1], axis=0)
+            error = gradient_f.dot(y - f_s)
+            delta = linalg.inv(H_f).dot(error)
+            curr = prev + delta
+            iterations += 1
+            if functional(curr) < functional(prev):
+                break
+            else:
+                lr = lr * factor ** k
+        if stop_condition(prev, curr):
+            break
+        else:
+            prev = curr
+    return curr, iterations
 
 
 def main():
-    functionals = [functional_linear, functional_rational]
+    functionals = [(functional_linear, functional_linear2), (functional_rational, functional_linear2)]
     regressions = [('linear', linear_reg_f), ('rational', rational_reg_f)]
+    jac = lambda point: np.array([float(diff(functionals[i][1], (point[0], point[1]), (1, 0))),
+                                  float(diff(functionals[i][1], (point[0], point[1]), (0, 1)))])
     for i in range(len(functionals)):
-        optimal_gd, gd_iterations = steepest_gradient_descent(functionals[i])
-        # optimal_conjugate, con_iterations = conjugate_gradient_method(functionals[i])
-        python_nm_res = sp.minimize(functionals[i],
-                                    np.array((np.random.rand(1)[0], np.random.rand(1)[0])),
-                                    method="CG",
-                                    options={'gtol': 1e-3})
-        optimal_newton, newton_iterations = newton_method(functionals[i])
-        optimal_lm, lm_iterations = levenberg_marquardt_algorithm(functionals[i])
-        # plot_dataset(regressions[i], optimal_gd, optimal_conjugate, optimal_newton, optimal_lm)
+        optimal_gd, gd_iterations = steepest_gradient_descent(functionals[i][1])
+        # optimal_gd, gd_iterations = gradient_descent(functionals[i][1]) # долго сходится
+        python_cg_res = sp.minimize(functionals[i][0],
+                                     np.array((np.random.rand(1)[0], np.random.rand(1)[0])),
+                                     method="CG",
+                                     options={'gtol': epsilon})
+        optimal_cg = python_cg_res.x
+        cg_iterations = python_cg_res.nit
+        # optimal_newton, newton_iterations = newton_method(functionals[i])
+        newton_res = sp.minimize(functionals[i][0],
+                                 np.array((np.random.rand(1)[0], np.random.rand(1)[0])),
+                                 jac=jac,
+                                 method='Newton-CG',
+                                 options={'gtol': epsilon})
+        optimal_newton = newton_res.x
+        newton_iterations = newton_res.nit
+        optimal_lm, lm_iterations = levenberg_marquardt_algorithm(functionals[i][0], regressions[i][1])
+        # python_lm_res = sp.least_squares(functionals[i][0],
+        #                                  x0=np.array((np.random.rand(1)[0], np.random.rand(1)[0])),
+        #                                  jac=jac,
+        #                                  method="lm",
+        #                                  xtol=epsilon)
+        # - не работает, ибо почему-то компилятор воспринимает размерность x0 как 2, а не 1 (хотя это кортеж),
+        #  падает, когда пытается эту пару аргументов отправить на вход функционалу, который принимает 1 аргумент.
+        plot_dataset(regressions[i], optimal_gd, optimal_cg, optimal_newton, optimal_lm)
         print("coefficients initial: ", alpha[0], betta[0])
-        # print("coefficients with Gradient Descent method (for {} regression) ({} iterations): "
-        #       .format(regressions[i][0], gd_iterations), optimal_gd[0], optimal_gd[1])
-        # print("coefficients with Conjugate Gradient Descent method (for {} regression) ({} iterations): "
-        #       .format(regressions[i][0], con_iterations), optimal_conjugate[0], optimal_conjugate[1])
-        # print("coefficients with Newton method (for {} regression) ({} iterations): "
-        #       .format(regressions[i][0], newton_iterations), optimal_newton[0], optimal_newton[1])
-        # print("coefficients with Levenberg-Marquardt algorithm (for {} regression) ({} iterations): "
-        #       .format(regressions[i][0], lm_iterations), optimal_lm[0], optimal_lm[1])
+        print("coefficients with Gradient Descent method (for {} regression) ({} iterations): "
+          .format(regressions[i][0], gd_iterations), optimal_gd[0], optimal_gd[1])
+        print("coefficients with Conjugate Gradient Descent method (for {} regression) ({} iterations): "
+           .format(regressions[i][0], cg_iterations), optimal_cg[0], optimal_cg[1])
+        print("coefficients with Newton method (for {} regression) ({} iterations): "
+          .format(regressions[i][0], newton_iterations), optimal_newton[0], optimal_newton[1])
+        print("coefficients with Levenberg-Marquardt algorithm (for {} regression) ({} iterations): "
+          .format(regressions[i][0], lm_iterations), optimal_lm[0], optimal_lm[1])
         print()
 
 
--- Scripts/utility.py
+++ Scripts/utility.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 epsilon = 0.001
+gd_epsilon = 0.0001
 alpha = np.random.rand(1)
 betta = np.random.rand(1)
 
@@ -51,20 +52,28 @@ def golden_section(functional, interval):
 
 def plot_dataset(regression, optimal_gd, optimal_conjugate, optimal_newton, optimal_lm):
     x = dataset[0]
-    result_optimal_gd = regression[1](x, np.array(optimal_gd[0]), np.array(optimal_gd[1]))
-    # result_optimal_conjugate = regression[1](x, np.array(optimal_conjugate[0]), np.array(optimal_conjugate[1]))
-    # result_optimal_newton = regression[1](x, np.array(optimal_newton[0]), np.array(optimal_newton[1]))
-    # result_optimal_lm = regression[1](x, np.array(optimal_lm[0]), np.array(optimal_lm[1]))
+    result_optimal_gd = regression[1](np.array(optimal_gd[0]), np.array(optimal_gd[1]))
+    result_optimal_conjugate = regression[1](np.array(optimal_conjugate[0]), np.array(optimal_conjugate[1]))
+    result_optimal_newton = regression[1](np.array(optimal_newton[0]), np.array(optimal_newton[1]))
+    result_optimal_lm = regression[1](np.array(optimal_lm[0]), np.array(optimal_lm[1]))
     plt.plot(x, dataset[1], 'o')
     plt.xlabel('x')
     plt.ylabel('y')
     if regression[0] == 'linear':
-        initial = regression[1](x, alpha, betta)
+        initial = regression[1](alpha, betta)
         plt.plot(x, initial, label='initial')
     plt.plot(x, result_optimal_gd, label='gradient descent')
-    # plt.plot(x, result_optimal_conjugate, label='conjugate gradient descent')
-    # plt.plot(x, result_optimal_newton, label='Newton method')
-    # plt.plot(x, result_optimal_lm, label='Levenberg Marquardt')
+    plt.plot(x, result_optimal_conjugate, label='conjugate gradient descent')
+    plt.plot(x, result_optimal_newton, label='Newton method')
+    plt.plot(x, result_optimal_lm, label='Levenberg Marquardt')
     plt.legend(framealpha=1, frameon=True)
     plt.title('plot of {} regression'.format(regression[0]))
+    plt.show()
+
+
+def plot_functional_history(iterations, functional_history):
+    fig, ax = plt.subplots(figsize=(12, 8))
+    ax.set_ylabel('Value of functional')
+    ax.set_xlabel('Iterations')
+    _ = ax.plot(range(iterations), np.array(functional_history), 'b.')
     plt.show()
